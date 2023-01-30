e406b9e92b7a657cf9390688a5ae0effb8b18893
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2023-01-30 03:07:20 +0000

    Window 1 completed

--- gui.py
+++ gui.py
@@ -1,26 +1,17 @@
 # Program helps user choose a phone based on their needs and budgets
 # Gui has yes and no button and buttons for phone brands and buttons for their price range and preferences 
 import tkinter as tk
-window = tk.Tk()
-window.geometry("500x500")
-window.title("Phone Selector")
-window.mainloop()
+window1 = tk.Tk()
+window1.geometry("500x500") #Used to configure our window size 
+window1.configure(bg='#17202A') #This is used to select background color of our window
 
-
-
-
-
-
-
-
-
-
-
-# window = Tk()
-# l1 = Label(window,text="Yes")
-# l1.grid(row=1, column = 6)
-# l2 = Label(window,text="No")
-# l1.grid(row=3, column = 6)
-# yes_text = StringVar()
-# e1 =  Entry(window,textvariable=yes_text)
-# window.mainloop()
+#This is how we name our title
+window1.title("Phone Selector")
+Welcome = tk.Label(window1,text="Welcome to the phone selector! Do you have have a phone brand in mind?")
+# label.pack()
+Welcome.place(x=1,y=1,height=15,width=500) #This is used to posotion the welcome prompt
+yes=tk.Button(window1, text="Yes")#this is our yes button
+yes.place(x=170,y=30)#this is used to position our yes button
+no=tk.Button(window1, text="No")
+no.place(x=250,y=30)
+window1.mainloop() #tells Python to run the Tkinter event loop
