dc97e91f57db34a482d01067f80ed787dd7f503b
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2023-01-30 00:14:42 +0000

    First GUI

---
+++ gui.py
@@ -0,0 +1,26 @@
+# Program helps user choose a phone based on their needs and budgets
+# Gui has yes and no button and buttons for phone brands and buttons for their price range and preferences 
+import tkinter as tk
+window = tk.Tk()
+window.geometry("500x500")
+window.title("Phone Selector")
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+# window = Tk()
+# l1 = Label(window,text="Yes")
+# l1.grid(row=1, column = 6)
+# l2 = Label(window,text="No")
+# l1.grid(row=3, column = 6)
+# yes_text = StringVar()
+# e1 =  Entry(window,textvariable=yes_text)
+# window.mainloop()
